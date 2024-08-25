4a552a97490cba4965de7548019adf79efb5102e
Akash <akashi.gamedev@gmail.com>
2024-08-26 01:55:20 +0530

    whoweare section responsive css complete

--- src/components/whoweare/WhoWeAre.js
+++ src/components/whoweare/WhoWeAre.js
@@ -1,45 +1,39 @@
+import React from "react";
 import styles from "./WhoWeAre.module.css";
-const WhoWeAre = () => (
-  <div>
-    <h1 className={styles.about}>ABOUT US</h1>
-    <div className={styles.gparent}>
-      <div className={styles.parent}>
-        <div className={styles.container}>
-          <div className={styles.child}>
-            <h3>Who we are?</h3>
-            <h4>
-              Home Building Associates is a team of architects, civil engineers,
-              interior designers and a lot of software engineers who love
-              designing, who love construction, who love seeing a idea turn into
-              a beautiful home. We are working this field in the last 15 years.
-              We are based on Kanpur, Uttar Pradesh Company and providing Our
-              Services in all over India with very reasonable Costing.
-            </h4>
-          </div>
-          <div className={styles.img1}>
-            <img src="/assets/images/2.png" alt="this is an image" />
-          </div>
-        </div>
 
-        <div className={styles.container}>
-          <div className={styles.img1}>
-            <img src="/assets/images/3.png" alt="this is an image" />
-          </div>
-          <div className={styles.child}>
-            <h3>Our mission</h3>
-            <h4>
-              One of the biggest achievements in life is considered to be
-              building one’s own house. Whether you are building a home on a
-              plot or have recently purchased an apartment flat, Home Building
-              Associates (HBA) helps you with all your planning and design needs
-              without needing a time consuming and expensive visit to an
-              engineer or a designer.
-            </h4>
-          </div>
+const WhoWeAre = () => {
+  return (
+    <div className={styles.container}>
+      <h1>About us</h1>
+      <section>
+        <div className={styles.textSection}>
+          <h2>Who we are?</h2>
+          <p>
+            Home Building Associates is a team of architects, civil engineers,
+            interior designers and a lot of software engineers who love
+            designing, who love construction, who love seeing a idea turn into a
+            beautiful home. We are working this field in the last 15 years. We
+            are based on Kanpur, Uttar Pradesh Company and providing Our
+            Services in all over India with very reasonable Costing.
+          </p>
         </div>
-      </div>
+        <img src="/assets/images/2.png" alt="" />
+      </section>
+      <section>
+        <div className={styles.textSection}>
+          <h2>Our Mission</h2>
+          <p>
+            One of the biggest achievements in life is considered to be building
+            one's own house. Whether you are building a home on a plot or have
+            recently purchased an apartment flat, Home Building Associates (HBA)
+            helps you with all your planning and design needs without needing a
+            time consuming and expensive visit to an engineer or a designer.
+          </p>
+        </div>
+        <img src="/assets/images/2.png" alt="" />
+      </section>
     </div>
-  </div>
-);
+  );
+};
 
 export default WhoWeAre;
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -3,11 +3,13 @@ import WhoWeAre from "../../components/whoweare/WhoWeAre";
 import Card from "../../components/card/Card";
 import Leader from "../../components/leaders/Leaders";
 import Achivements from "../../components/achievement/Achievements";
-
+import Spacer from '../../components/Spacer';
 export default function About() {
   return (
     <div>
+      <Spacer size={'small'} />
       <WhoWeAre />
+      <Spacer />
       <Card />
       <Leader />
       <Achivements />
