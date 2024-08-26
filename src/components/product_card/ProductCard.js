d87f84262e90c84d9f4871e157cd37e56bf6e91d
Akash <akashi.gamedev@gmail.com>
2024-08-26 13:07:34 +0530

    homepage responsive css complete

--- src/components/product_card/ProductCard.js
+++ src/components/product_card/ProductCard.js
@@ -4,7 +4,7 @@ import styles from "./ProductCard.module.css";
 export default function ProductCard({ name, description, image, style }) {
   return (
     <div className={`${styles.container} ${style}`}>
-      <img src={image} alt="Interior Product" />
+      <img src={image} alt="Product" />
       <div className={styles.textSection}>
         <h4>{name}</h4>
         <p>{description}</p>
